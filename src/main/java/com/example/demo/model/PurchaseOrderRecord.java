057a540afe43c13ab994a3432cd35357dd2b06b5
bala-amypo <bala@amypo.in>
2025-12-18 04:43:46 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseOrderRecord.java
+++ src/main/java/com/example/demo/model/PurchaseOrderRecord.java
@@ -1,8 +1,20 @@
 package com.example.demoo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
 @Entity
 public class PurchaseOrderRecord{
 
-
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true)
+    private String poNumber;
+    private Long supplierld;
+    private String itemDescription;
+    private Integer quantity;
+    private String promisedDeliveryDate;
+    private String issuedDate;
 }
