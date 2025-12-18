2a81ccd888986a06268172d073e11520d3179fb4
bala-amypo <bala@amypo.in>
2025-12-18 06:23:41 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryRecord.java
+++ src/main/java/com/example/demo/model/DeliveryRecord.java
@@ -6,5 +6,9 @@ package com.example.demo.model;
 @Entity
 public class DeliveryRecord{
     @Id
-    @GeneratedValue(strategy=)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Long pold;
+    private String actualDeliveryDate;
+    private Long 
 }
