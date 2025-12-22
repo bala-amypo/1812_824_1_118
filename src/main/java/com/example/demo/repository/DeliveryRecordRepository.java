e43a9243a7a5329a9bc2675ee5ea540bf43fd254
bala-amypo <bala@amypo.in>
2025-12-22 01:24:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.DeliveryRecord;
+import java.util.List;
 
 public interface DeliveryRecordRepository extends JpaRepository<DeliveryRecord,Long>{
+    List<DeliveryRecord> getDeliveriesByPO(Long poId);
     
 }
