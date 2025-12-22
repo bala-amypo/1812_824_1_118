fe7e0a030f6ecb45b36dc713c0b095d7dcce7d85
bala-amypo <bala@amypo.in>
2025-12-22 01:57:34 +0000

    User commit

--- src/main/java/com/example/demo/model/DelayScoreRecord.java
+++ src/main/java/com/example/demo/model/DelayScoreRecord.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.time.LocalDate;
+import jakarta.persistence.Column;
 
 
 @Entity
