710f6034f626722d63afa94fa9f6af5374f2e05a
bala-amypo <bala@amypo.in>
2025-12-22 01:51:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/DelayScoreRecordRepository.java
+++ src/main/java/com/example/demo/repository/DelayScoreRecordRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.DelayScoreRecord;
-import java.util.LIst;
+import java.util.List;
 
 public interface DelayScoreRecordRepository extends JpaRepository<DelayScoreRecord,Long>{
     List<DelayScoreRecord> findBySupplierId(Long supplierId);
