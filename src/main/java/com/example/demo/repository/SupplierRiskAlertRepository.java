19ca922789d374c129aacf911c5bb7386baa0acf
bala-amypo <bala@amypo.in>
2025-12-22 02:27:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplierRiskAlertRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRiskAlertRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.SupplierRiskAlert;
+import java.util.List;
 
 public interface SupplierRiskAlertRepository extends JpaRepository<SupplierRiskAlert,Long>{
-    
+    List<SupplierRiskAlert> findBySupplierId(Long supplierId);
 }
---
+++ src/main/java/com/example/demo/service/impl/AuthServiceImmpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public AppUser registerUser(AppUser user) {
+        return repo.save(user);
+    }
+
+    @Override
+    public AppUser findByUsername(String username) {
+        return repo.findByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return repo.existsByEmail(email);
+    }
+}
