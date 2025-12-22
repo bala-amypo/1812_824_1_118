24b5ec7d208251e26e82e48f7e0aff9e39a45fb4
bala-amypo <bala@amypo.in>
2025-12-22 02:39:34 +0000

    User commit

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -1,17 +1,3 @@
-package com.example.demo.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.JoinColumn;
-
-import java.time.LocalDateTime;
-import java.util.Set;
-
 @Entity
 public class AppUser {
 
@@ -19,22 +5,34 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String username;
     private String email;
-
     private String password;
     private String roles;
-
     private String createdAt;
+
+    // Getters & Setters
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -42,6 +40,7 @@ public class AppUser {
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -49,6 +48,7 @@ public class AppUser {
     public String getRoles() {
         return roles;
     }
+
     public void setRoles(String roles) {
         this.roles = roles;
     }
@@ -56,19 +56,20 @@ public class AppUser {
     public String getCreatedAt() {
         return createdAt;
     }
+
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
 
-    public AppUser(Long id,String email,String password,String roles,String createdAt) {
+    public AppUser() {}
 
+    public AppUser(Long id, String username, String email,
+                   String password, String roles, String createdAt) {
         this.id = id;
+        this.username = username;
         this.email = email;
         this.password = password;
         this.roles = roles;
         this.createdAt = createdAt;
-    }
-    public AppUser(){
-    
     }
 }
