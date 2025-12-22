c7efbeab4f6466604c25679580479c8696fd76fb
bala-amypo <bala@amypo.in>
2025-12-22 05:18:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/PurchaseOrderController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.PurchaseOrderRecord;
+import com.example.demo.service.PurchaseOrderService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/purchase-orders")
+@Tag(name = "Purchase Orders")
+public class PurchaseOrderController {
+
+    @Autowired
+    private PurchaseOrderService service;
+
+    @PostMapping
+    public PurchaseOrderRecord create(@RequestBody PurchaseOrderRecord po) {
+        return service.createPO(po);
+    }
+
+    @GetMapping("/supplier/{supplierId}")
+    public List<PurchaseOrderRecord> getBySupplier(@PathVariable Long supplierId) {
+        return service.getPOsBySupplier(supplierId);
+    }
+
+    @GetMapping("/{id}")
+    public PurchaseOrderRecord getById(@PathVariable Long id) {
+        return service.getPOById(id);
+    }
+
+    @GetMapping
+    public List<PurchaseOrderRecord> getAll() {
+        return service.getAllPOs();
+    }
+}
