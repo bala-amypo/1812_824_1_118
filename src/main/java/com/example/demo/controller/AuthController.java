88e1211a93a14bc1dde11c33aa3949797a16b35f
bala-amypo <bala@amypo.in>
2025-12-22 19:31:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.example.demo.model.AppUser;
 import com.example.demo.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,17 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Authentication")
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+    private final AuthService authService;
+
+    // Constructor Injection (MANDATORY)
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/register")
     public AppUser register(@RequestBody AppUser user) {
-        return authService.register(user);
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody AppUser user) {
-        return authService.login(user.getEmail(), user.getPassword());
+        return authService.registerUser(user);
     }
 }
-~
