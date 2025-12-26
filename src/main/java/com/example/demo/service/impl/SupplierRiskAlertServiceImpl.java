208587f39933dd847e3a40a48c5ebcaf0f266f11
bala-amypo <bala@amypo.in>
2025-12-26 09:19:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplierRiskAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplierRiskAlertServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.SupplierRiskAlert;
 import com.example.demo.repository.SupplierRiskAlertRepository;
 import com.example.demo.service.SupplierRiskAlertService;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SupplierRiskAlertServiceImpl implements SupplierRiskAlertService {
