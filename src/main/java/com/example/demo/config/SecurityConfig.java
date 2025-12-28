f94e5414ed809c7595218c142161bc2299a9e558
bala-amypo <bala@amypo.in>
2025-12-28 19:21:25 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
             .requestMatchers(
                 "/api/auth/**",
                 "/swagger-ui/**",
-                "/v3/api-docs/**"
+                "/v3/api-docs/**",
+                "/api/suppliers"
             ).permitAll()
             .anyRequest().authenticated()
         );
